2f9da838d69d14b9e5a50290323bc539fb2f4123
Nadatari <dariosting@gmail.com>
2021-07-16 20:02:11 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,93 @@
+/*    Images from Unsplash
+
+        Mark Basarab (bear image): url(https://unsplash.com/photos/y421kXlUOQk),
+        Max van den Oetelaar (tiger image): url(https://unsplash.com/photos/S0txA-JnUFA),
+        Sam Carter (sheep image): url(https://unsplash.com/photos/GHOiyov2TSQ),
+        Philipp Pilz (wolf image): url(https://unsplash.com/photos/QZ2EQuPpQJs),
+        Mohan Moolepetlu (snake image): url(https://unsplash.com/photos/VUr5nmC1IM4),
+        Andrea Chioldin (deer image): url(https://unsplash.com/photos/6LChsc9M2rA),
+        Keyang Zheng (doe image): url(https://unsplash.com/photos/tY4Op7DdBrc),
+        Christopher Carson (piglet image): url(https://unsplash.com/photos/i4XLJmlYit4)
+*/
+
+let buttonCarni = document.getElementById("btn-carni");
+let buttonHerbi = document.getElementById("btn-herbi");
+let buttonOmni = document.getElementById("btn-omni");
+let buttonAll = document.getElementById("btn-all");
+let images = document.querySelectorAll(".image-container > img");
+let searchText = document.getElementById("inputs");
+
+//Button Logic
+buttonCarni.onclick = () => {
+  images.forEach((e) => {
+    if (e.dataset.animal !== "carnivore") {
+      e.classList.add("hide");
+      buttonCarni.disabled = true;
+      buttonHerbi.disabled = true;
+      buttonOmni.disabled = true;
+      searchText.disabled = true;
+    }
+  });
+};
+
+buttonOmni.onclick = () => {
+  images.forEach((e) => {
+    if (e.dataset.animal !== "omnivore") {
+      e.classList.add("hide");
+      buttonCarni.disabled = true;
+      buttonHerbi.disabled = true;
+      buttonOmni.disabled = true;
+      searchText.disabled = true;
+    }
+  });
+};
+
+buttonHerbi.onclick = () => {
+  images.forEach((e) => {
+    if (e.dataset.animal !== "herbivore") {
+      e.classList.add("hide");
+      buttonCarni.disabled = true;
+      buttonHerbi.disabled = true;
+      buttonOmni.disabled = true;
+      searchText.disabled = true;
+    }
+  });
+};
+
+buttonAll.onclick = () => [
+  images.forEach((e) => {
+    if (e.classList.contains("hide")) {
+      e.classList.remove("hide");
+      buttonCarni.disabled = false;
+      buttonHerbi.disabled = false;
+      buttonOmni.disabled = false;
+      searchText.disabled = false;
+    }
+    if (searchText.value.length > 0) {
+      searchText.value = "";
+    }
+  }),
+];
+
+//Input Logic
+const search = () => {
+  if (searchText.value.length > 0) {
+    images.forEach((image) => {
+      image.classList.add("hide");
+      let miniSearchText = searchText.value.toLowerCase();
+      if (image.dataset.animal.indexOf(miniSearchText) > -1) {
+        image.classList.remove("hide");
+        buttonCarni.disabled = true;
+        buttonHerbi.disabled = true;
+        buttonOmni.disabled = true;
+      }
+    });
+  } else {
+    images.forEach((image) => {
+      image.classList.remove("hide");
+      buttonCarni.disabled = false;
+      buttonHerbi.disabled = false;
+      buttonOmni.disabled = false;
+    });
+  }
+};
